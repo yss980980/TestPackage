cd524bbe6679163055908b330cf87df1d99474b0
yangshaoshuai <478094413@qq.com>
2022-04-24 23:10:50 +0800

    package首次提交测试

---
+++ Packages/CustomPackage/Runtime/TestPack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPack : MonoBehaviour
+{
+    public static TestPack Instance;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    public void Hello()
+    {
+        Debug.Log("你好");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Hello();
+        }
+    }
+}
